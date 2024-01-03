587c75ca0a0c382d5c6c62197e93ab52a50217df
Emanuel B <50232770+TheOwlbird@users.noreply.github.com>
2024-01-03 21:14:20 +0100

    Minor Compiler Update

---
+++ Pano/Assets/Scripts/Demo_MoveInLine.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demo_MoveInLine : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 0f;
+    [SerializeField] private bool invertDirection = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var m = movementSpeed*Time.deltaTime;
+
+        if(invertDirection)
+        {
+            m*=-1;
+        }
+
+        this.transform.position = this.transform.position + new Vector3(m, 0f, 0f);
+    }
+}
